74da2e6d0465de1c6be7c08d3a21707d01bf8b56
Alexander Curtin <alexcurtin@Alexanders-MBP.fios-router.home>
2019-08-14 18:45:46 -0400

    registering adds auth token to local storage

--- record-app/app/controllers/users_controller.rb
+++ record-app/app/controllers/users_controller.rb
@@ -14,9 +14,9 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-
     if @user.save
-      render json: @user, status: :created, location: @user
+      token = encode(id: @user.id, username: @user.username)
+      render json: {token: token, user: @user}, status: :created, location: @user
     else
       render json: @user.errors, status: :unprocessable_entity
     end
