1f1235067aeb55786c216ce1d68dc1f52a087bd4
Alexander Curtin <alexcurtin@Alexanders-MacBook-Pro.local>
2019-08-19 11:02:05 -0400

    minor styling

--- record-app/client/src/components/RecordsList.jsx
+++ record-app/client/src/components/RecordsList.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, Route, withRouter } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
 
 class RecordsList extends React.Component {
   constructor(props) {
